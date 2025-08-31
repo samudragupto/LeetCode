8e8d08fc6f4b885142947655341eacb3810d6e20
Arrhat <arrhatnag1@gmail.com>
2025-09-01 01:27:15 +0530

    Create Replace Non-Coprime Numbers in Array.py

---
+++ Replace Non-Coprime Numbers in Array.py
@@ -0,0 +1,13 @@
+import math
+
+class Solution:
+    def replaceNonCoprimes(self, nums):
+        stack = []
+        
+        for num in nums:
+            while stack and math.gcd(stack[-1], num) > 1:
+                top = stack.pop()
+                num = (top * num) // math.gcd(top, num)
+            stack.append(num)
+        
+        return stack
