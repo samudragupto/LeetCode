fedfa9b7b31af1cc007fb2a7514f29cb45fdc715
Arrhat <arrhatnag1@gmail.com>
2025-09-01 01:51:42 +0530

    Create Sliding Window Maximum.py

---
+++ Sliding Window Maximum.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+class Solution:
+    def maxSlidingWindow(self, nums, k):
+        dq = deque()
+        result = []
+        
+        for i, num in enumerate(nums):
+            while dq and nums[dq[-1]] < num:
+                dq.pop()
+            dq.append(i)
+            
+            if dq[0] == i - k:
+                dq.popleft()
+            
+            if i >= k - 1:
+                result.append(nums[dq[0]])
+        
+        return result
