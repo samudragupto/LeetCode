f8a0cc65ed0e2af8956a8e73eb5a6326a154b424
Arrhat <arrhatnag1@gmail.com>
2025-09-09 15:41:53 +0530

    Create Fruit Into Baskets.py

---
+++ Fruit Into Baskets.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        basket = {}
+        left = 0
+        max_fruits = 0
+
+        for right in range(len(fruits)):
+            basket[fruits[right]] = basket.get(fruits[right], 0) + 1
+
+            while len(basket) > 2:
+                basket[fruits[left]] -= 1
+                if basket[fruits[left]] == 0:
+                    del basket[fruits[left]]
+                left += 1
+
+            max_fruits = max(max_fruits, right - left + 1)
+
+        return max_fruits
